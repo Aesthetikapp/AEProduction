dc408447faf634a9ce5a83f22c72582eb6098b69
LexiconInfotech <36230688+LexiconInfotech@users.noreply.github.com>
2023-04-14 19:48:00 +0530

    push notification
    push notification

--- back-end/models/treatmenttype.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
-
-const treatmenttypeSchema = new Schema({
-    treatmenttype: String,
-    bodyarea: String,
-}, { collection: 'treatmenttype' });
-
-module.exports = mongoose.model('treatmenttype', treatmenttypeSchema);
--- pushnotifications/back-end/models/allergies.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const Allergieschema = new Schema({
-	allergyname: String,
-	allergyDescription: String,
-});
-
-module.exports = mongoose.model("allergies", Allergieschema);
