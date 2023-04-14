9baafce1e91ecfd8de2f61874b02e4c8a4e18d31
LexiconInfotech <36230688+LexiconInfotech@users.noreply.github.com>
2023-04-14 19:39:52 +0530

    NewChanges
    NewChanges

--- pushnotifications/front-end-src/common/stringutility.js
+++
@@ -1,4 +0,0 @@
-// 👇️ if you need to capitalize first and lowercase rest
-export const capitalizeFirstLowercaseRest = (str) => {
-	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
